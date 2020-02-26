f44f615a1339d14a4c76eedb9a51a8e94ebd4db8
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-02-26 22:32:00 +0100

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 PATH="$PATH:/home/builder/.dotnet/tools"
+if [ -z $SONARHOST ] ; then
+SONARHOST = "51.158.78.1"
+SONARKEY = "8053be8e6cdf21e7e46dd3664e7ce948cf502298"
+fi
 cd ${PWD}/samples/complexapp
-dotnet sonarscanner begin /k:dotNet /d:sonar.login=8053be8e6cdf21e7e46dd3664e7ce948cf502298 /d:sonar.host.url="http://51.158.78.1:9000"
+dotnet sonarscanner begin /k:dotNet /d:sonar.login=$SONARKEY /d:sonar.host.url="http://$SONARHOST:9000"
 dotnet build .
-dotnet sonarscanner end  /d:sonar.login=8053be8e6cdf21e7e46dd3664e7ce948cf502298
+dotnet sonarscanner end  /d:sonar.login=$SONARKEY
